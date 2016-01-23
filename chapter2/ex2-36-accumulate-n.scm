29d2d8e0ef09d43e8a23d4029c2211a849774239
ke-pan <ke_pan@yeah.net>
2016-01-23 15:25:23 +0800

    exercise 2.36

---
+++ chapter2/ex2-36-accumulate-n.scm
@@ -0,0 +1,11 @@
+(load "accumulate.scm")
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+    `()
+    (cons (accumulate op init (map car seqs))
+          (accumulate-n op init (map cdr seqs)))))
+
+(define x (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+(map car x)
+(map cdr x)
+(accumulate-n + 0 x)
